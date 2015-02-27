633594a403f7b4dab20d8d367b74815f47cd6f11
Jack Mallers <jimmymowschess@gmail.com>
2015-02-27 16:07:53 -0600

    trying to get prod working

--- app/components/video-chat.js
+++ app/components/video-chat.js
@@ -5,19 +5,19 @@ export default Ember.Component.extend({
   classNames: ['video-chat'],
   apiKey: "44827272",
   didInsertElement: function() {
-    var guestCounter = 1;
+    // var guestCounter = 1;
     var session = OT.initSession(this.get('apiKey'), this.get('sessionId'));
 
     session.on("streamCreated", function(event) {
-      Ember.$('.video-chat').prepend("<div id='guestPublisher" + guestCounter + "' class='video-box'></div>");
-      session.subscribe(event.stream, "guestPublisher" + guestCounter, {width: 200, height: 159});
-      guestCounter++;
-    });
+      // this.$('.video-chat').prepend("<div id='guestPublisher" + guestCounter + "' class='video-box'></div>");
+      session.subscribe(event.stream, "otherPeople", { width: 200, height: 159, insertMode: 'append', style: { buttonDisplayMode: 'off', nameDisplayMode: 'off', bugDisplayMode: 'off' } });
+      // guestCounter++;
+    }.bind(this));
 
     session.connect(this.get('token'), function() {
       var publisher = OT.initPublisher("youPublisher", {width: 200, height: 159});
-      Ember.$("#youPublisher").prependTo(".video-chat");
+      this.$("#youPublisher").prependTo(".video-chat");
       session.publish(publisher);
-    });
+    }.bind(this));
   }
 });
