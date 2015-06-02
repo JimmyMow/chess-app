5a6055db03538834e685e359058374dec248ac6f
Jack Mallers <jimmymowschess@gmail.com>
2015-06-02 18:58:32 -0500

    added editing puzzles on front and back end. needs some styling

--- app/components/user-puzzle.js
+++ app/components/user-puzzle.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Notify from 'ember-notify';
 
 export default Ember.Component.extend({
   classNames: ['puzzle-container'],
@@ -7,16 +8,17 @@ export default Ember.Component.extend({
   fen: null,
   gameFen: null,
   board: null,
+  turn: null,
   fenData: {
     toPlay: null,
     whiteKingCastles: '-',
     whiteQueenCastles: '-',
     blackKingCastles: '-',
-    blackQueenCastles: '-',
+    blackQueenCastles: '-'
   },
   whiteToPlay: function() {
-    return this.get('fenData').toPlay === 'w'
-  }.property('fenData'),
+    return this.get('turn') === 'w';
+  }.property('turn'),
   actions: {
     edit: function() {
       var controller = this.get('targetObject');
@@ -34,6 +36,36 @@ export default Ember.Component.extend({
             free: false
           }
         });
+        var name = this.get('puzzle.name');
+        var fen = this.get('board').getFen();
+        var gamePos = this.get('computeFen')(this, this.get('fenData'), fen);
+        this.set('puzzle.name', name);
+        this.set('puzzle.position', fen);
+        this.set('puzzle.gameFen', gamePos);
+        var data = {
+          "position": fen,
+          "gameFen": gamePos,
+          "name": name
+        };
+        Ember.$.ajax({
+          url: '/api/puzzles/' + this.get('puzzle.id'),
+          contentType: 'application/json',
+          data: JSON.stringify({ puzzle: data }),
+          dataType: "json",
+          type: 'PUT'
+        }).then(function(response) {
+          if(response) {
+            Notify.success({
+              raw: "<div>Successfully updated your puzzle</div>",
+              closeAfter: 3000
+            });
+          } else {
+            Notify.error({
+              raw: "<div>Shoot, there was a problem editing your puzzle</div>",
+              closeAfter: 3000
+            });
+          }
+        });
       }
     },
     parseFen: function() {
@@ -42,6 +74,7 @@ export default Ember.Component.extend({
 
       var tokens = fen.split(/\s+/);
       fenData.toPlay = tokens[1];
+      this.set('turn', tokens[1]);
 
       if (tokens[2].indexOf('K') > -1) {
         fenData.whiteKingCastles = 'K';
@@ -57,11 +90,32 @@ export default Ember.Component.extend({
       }
     }
   },
+  computeFen: function(component, fenData, fen) {
+    var finalCastleData = component.get('computeFenCastles')(fenData);
+    var res = fen + ' ' + fenData.toPlay + ' ' + finalCastleData + ' - 0 1';
+    return res;
+  },
+  computeFenCastles: function(data) {
+    var castles = '';
+    Object.keys(data).forEach(function(piece) {
+      if (piece !== 'toPlay' && data[piece]) {
+        castles += data[piece];
+      }
+    });
+    return castles.length ? castles : '-';
+  },
   didInsertElement: function() {
+    var _this = this;
     this.set('name', this.get('puzzle.name'));
-    this.set('fen', this.get('puzzle.fen'));
+    this.set('fen', this.get('puzzle.position'));
     this.set('gameFen', this.get('puzzle.gameFen'));
 
     this.send('parseFen');
+
+    Ember.$('.puzzle-container').on('change', '.puzzle-color', function() {
+      var val = Ember.$(this).val();
+      _this.get('fenData').toPlay = val;
+      _this.set('turn', val);
+    });
   }
 });
